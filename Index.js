039d7eb7c9e853b179065145ff29e4a9918a87d7
femboydoesstuffz <bryceprince16@gmail.com>
2025-10-15 13:36:58 -0400

    Index.js
    Index oooOOoOooo

---
+++ Index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, { cors: { origin: '*' } });
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg); // send message to everyone
+    });
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`MRP chat server running on port ${PORT}`));
